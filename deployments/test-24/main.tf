fd1d4c7eec019dd476924bdaf8f5ed42be5c15b7
Neil Chambers <neilc252@yahoo.com>
2025-11-06 20:13:06 -0600

    feat: Add Terraform configuration for test-24

--- deployments/test-24/main.tf
+++ deployments/test-24/main.tf
@@ -1,130 +1,52 @@
-# Configure the AzureRM Provider
-# The 'features' block must be empty as per critical instructions to avoid deprecation warnings.
-provider "azurerm" {
-  subscription_id = var.azure_subscription_id
-  features {}
+# Configure the Google Cloud provider
+# Ensure you have authenticated to GCP (e.g., using `gcloud auth application-default login`)
+# and set the default project if not explicitly specified in the provider block.
+provider "google" {
+  project = "umos-ab24d" # GCP Project ID from the configuration
+  region  = "us-central1" # Region for shared resources, instances use zones
 }
 
-# Data source to reference the existing Azure Resource Group.
-# The resource group is assumed to already exist and will not be created by Terraform.
-# This data source is named "rg" as per critical instructions.
-data "azurerm_resource_group" "rg" {
-  name = var.azure_resource_group_name
-}
+# Resource: Google Compute Instance (Virtual Machine)
+resource "google_compute_instance" "this_vm" {
+  # The name of the virtual machine instance, as specified in the configuration.
+  name         = "test-24"
+  # The machine type (e.g., n1-standard-1, e2-medium) for the VM.
+  machine_type = "n1-standard-1"
+  # The specific zone where the instance will be created.
+  # Instances require a zone, even if the region is specified in the provider.
+  # We're picking a common zone within the specified region.
+  zone         = "us-central1-a"
 
-# Generates an SSH private key that will be used for administrative access to the Linux VM.
-# The public key derived from this will be configured on the VM.
-resource "tls_private_key" "admin_ssh" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Creates a Virtual Network (VNet) for the VM.
-# This provides network isolation and a private IP address space.
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.instance_name}-vnet"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"] # A common address space for a new VNet
-}
-
-# Creates a subnet within the Virtual Network.
-# VMs will be deployed into this subnet.
-resource "azurerm_subnet" "internal" {
-  name                 = "${var.instance_name}-subnet"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"] # A common address prefix for a subnet
-}
-
-# Creates a Network Interface (NIC) for the VM.
-# This connects the VM to the subnet and assigns it an IP configuration.
-resource "azurerm_network_interface" "main" {
-  name                = "${var.instance_name}-nic"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic" # Assigns a private IP dynamically
-  }
-}
-
-# Deploys the Azure Linux Virtual Machine.
-# This resource is named "this_vm" as per critical instructions.
-resource "azurerm_linux_virtual_machine" "this_vm" {
-  name                = var.instance_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = "azureuser" # Standard admin username for Azure Linux VMs
-
-  # Connects the VM to the created Network Interface.
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-
-  # Configures SSH administrative access using the generated SSH key.
-  # Critically uses `public_key_openssh` from the tls_private_key resource, not `public_key_pem`.
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = tls_private_key.admin_ssh.public_key_openssh
+  # Boot disk configuration for the virtual machine.
+  boot_disk {
+    initialize_params {
+      # The custom image to use for the boot disk.
+      # This image name is expected to exist within the specified GCP project.
+      # Adhering to the "Actual Cloud Image Name" from the critical instructions.
+      image = "ubuntu-22-04-19155927176"
+    }
   }
 
-  # Defines the OS disk configuration for the VM.
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    disk_size_gb         = 30 # Default disk size for the OS disk
+  # Network interface configuration for the virtual machine.
+  network_interface {
+    # Connects the VM to the 'default' VPC network.
+    # A custom network/subnet could be specified here if required for specific networking setups.
+    network = "default"
+
+    # By default, a VM without an 'access_config' block in its network interface
+    # will only have an internal (private) IP address.
+    # To assign an external (public) IP, an 'access_config {}' block would be added.
+    # This script adheres to deploying only necessary resources and focusing on the private IP.
   }
 
-  # Specifies the source image for the VM.
-  # This uses a custom image identified by its full Azure Resource ID path.
-  source_image_id = "/subscriptions/${var.azure_subscription_id}/resourceGroups/${data.azurerm_resource_group.rg.name}/providers/Microsoft.Compute/images/${var.os_image_name}"
-
-  # Boot diagnostics are disabled as per "necessary resources only" instruction.
-  boot_diagnostics {
-    enabled = false
-  }
-
-  # User data/custom data is explicitly excluded as per critical instructions.
+  # Set deletion_protection to false as per the critical instructions.
+  # This allows the instance to be deleted without requiring manual intervention to disable protection.
+  deletion_protection = false
 }
 
-# Defines input variables for the Terraform configuration.
-variable "instance_name" {
-  description = "The name of the virtual machine instance."
-  type        = string
-  default     = "test-24" # Value from JSON configuration
-}
-
-variable "vm_size" {
-  description = "The size of the virtual machine."
-  type        = string
-  default     = "Standard_B1s" # Value from JSON configuration
-}
-
-variable "azure_resource_group_name" {
-  description = "The name of the existing Azure Resource Group where resources will be deployed."
-  type        = string
-  default     = "umos" # Value from JSON configuration
-}
-
-variable "azure_subscription_id" {
-  description = "The Azure Subscription ID where the resources will be deployed."
-  type        = string
-  default     = "c0ddf8f4-14b2-432e-b2fc-dd8456adda33" # Value from JSON configuration
-}
-
-variable "os_image_name" {
-  description = "The name of the custom OS image to use for the virtual machine."
-  type        = string
-  default     = "ubuntu-20-04-19123432891" # Actual cloud image name from instructions
-}
-
-# Output block for the private IP address of the deployed virtual machine.
-# This output is named "private_ip" as per critical instructions.
+# Output block to expose the private IP address of the created virtual machine.
 output "private_ip" {
   description = "The private IP address of the created virtual machine."
-  value       = azurerm_linux_virtual_machine.this_vm.private_ip_address
+  # For GCP, the private IP is found in the first network interface's network_ip attribute.
+  value       = google_compute_instance.this_vm.network_interface[0].network_ip
 }
